0130330e816b2a05182cdfd7085b0ab882c29d48
sexualizer <nikita-shchegolev@mail.ru>
2025-05-24 00:05:24 +0300

    kafka is working

--- app/kafka/consumer.py
+++ app/kafka/consumer.py
@@ -5,48 +5,47 @@ Date: 09.05.2025
 Project: Stealer
 """
 
-import json
 
-from kafka.consumer import KafkaConsumer
+from kafka import KafkaConsumer
 from datetime import datetime
 
-from app.utils.queries import QUERIES
-from app.utils.db import get_ch_client
+#from app.utils.queries import QUERIES
+#from app.utils.db import get_ch_client
 
 
 class Consumer:
     def __init__(self, topic: str = 'matches'):
         """KafkaConsumer Initial"""
         self.consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers='localhost:9092',
+            'test_topic',
+            bootstrap_servers='localhost:29092',
             auto_offset_reset='earliest',
-            value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-            api_version=(2, 5, 0))
+            value_deserializer=lambda x: x.decode('utf-8')
+        )
         self.test_conn()
-        self.ch_client = get_ch_client()
+        #self.ch_client = get_ch_client()
 
     def test_conn(self):
         try:
-            self.consumer.bootstrap_connected()
-            print("Connected to Kafka at localhost:9092")
+            for message in self.consumer:
+                print(f"Received: {message.value}")
         except Exception as e:
             print(f"Consumer - Caught exception while trying to connect to Kafka at localhost:9092: {e}")
 
-    def process_messages(self):
-        """Processing message from topic"""
-        for message in self.consumer:
-            try:
-                data = message.value
-                data['utc_date'] = datetime.fromisoformat(data['utc_date'])
-                data['last_updated'] = datetime.fromisoformat(data['last_updated'])
+    # def process_messages(self):
+    #     """Processing message from topic"""
+    #     for message in self.consumer:
+    #         try:
+    #             data = message.value
+    #             data['utc_date'] = datetime.fromisoformat(data['utc_date'])
+    #             data['last_updated'] = datetime.fromisoformat(data['last_updated'])
+    #
+    #             self.ch_client.execute(
+    #                 QUERIES['insert_matches'],
+    #                 [data]
+    #             )
+    #             print(f"[Kafka] Processed match ID: {data['id']}")
+    #         except Exception as e:
+    #             print(f"[Kafka] Processing error: {e}")
 
-                self.ch_client.execute(
-                    QUERIES['insert_matches'],
-                    [data]
-                )
-                print(f"[Kafka] Processed match ID: {data['id']}")
-            except Exception as e:
-                print(f"[Kafka] Processing error: {e}")
-
-consumer = Consumer(topic='matches')
+consumer = Consumer()
--- app/kafka/producer.py
+++ app/kafka/producer.py
@@ -1,56 +1,18 @@
-"""
-Kafka producer class.
-Author: sexualizer
-Date: 09.05.2025
-Project: Stealer
-"""
-
-import json
-
-from kafka import KafkaProducer, KafkaAdminClient
+from kafka import KafkaProducer
 from datetime import datetime
-from json import JSONEncoder
-
-
-class DateTimeEncoder(JSONEncoder):
-    """Class encodes JSON files"""
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return super().default(obj)
-
 
 class Producer:
-    """KafkaProducer Initial"""
-    def __init__(self, bootstrap_servers: str = 'localhost:9092'):
-        self.bootstrap_servers = bootstrap_servers
+    def __init__(self):
         self.producer = KafkaProducer(
-            bootstrap_servers=bootstrap_servers,
-            value_serializer=lambda v: json.dumps(v, cls=DateTimeEncoder).encode('utf-8'),
-            acks='all',
-            retries=3,
-            api_version=(2, 5, 0)
+            bootstrap_servers='localhost:29092',
+             value_serializer=lambda v: str(v).encode('utf-8')
         )
         self.test_conn()
 
     def test_conn(self):
-        """Test connection to Kafka"""
-        try:
-            admin_client = KafkaAdminClient(bootstrap_servers=self.bootstrap_servers)
-            topics = admin_client.list_topics()
-            print(topics)
-            print("Connected to Kafka at localhost:9092")
-        except Exception as e:
-            print(f"Producer - Caught exception while trying to connect to Kafka at localhost:9092: {e}")
+        msg = f'Test connection {datetime.now()}'
+        self.producer.send('test_topic', value=msg)
+        print(f'Sent:{msg}')
+        self.producer.flush()
 
-    def send_match(self, topic: str, data: dict) -> bool:
-        """Send message to topic"""
-        try:
-            future = self.producer.send(topic, value=data)
-            future.get(timeout=10)
-            return True
-        except Exception as e:
-            print(f"[Kafka] Send error: {e}")
-            return False
-
-#producer = Producer()
+producer = Producer()
